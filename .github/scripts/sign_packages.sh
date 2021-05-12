3472a6a55b5782bff8ee16f73e42d46db01dfa7c
mutantmonkey <mutantmonkey@mutantmonkey.in>
2021-05-12 02:52:21 -0700

    Remove signing stuff for now
    I'll have to rework this and add it back later so it runs with the
    correct uid.

--- .github/scripts/sign_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-install -d -m0700 /tmp/signify
-echo "$SIGNIFY_SECRET_KEY" > /tmp/signify/pkg.sec
-sha512sum *.pkg.tar.* > SHA512SUMS
-signify -S -s /tmp/signify/pkg.sec -m SHA512SUMS -x SHA512SUMS.sig <<< "$SIGNIFY_PASSWORD"
-rm -rf /tmp/signify
